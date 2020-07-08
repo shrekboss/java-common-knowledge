8398530c6e25b4405d21f143fd8d641adfd9e4fb
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-07-08 12:42:01 +0800

    code review

--- design-parent/src/main/java/org/bytedancer/crayzer/design_mode_thingking/polymorphism/array/SortedDynamicArray.java
+++ design-parent/src/main/java/org/bytedancer/crayzer/design_mode_thingking/polymorphism/array/SortedDynamicArray.java
@@ -7,14 +7,14 @@ public class SortedDynamicArray extends DynamicArray {
     public void add(Integer e) {
         ensureCapacity();
         int i;
-        for (i = size-1; i>=0; --i) { //保证数组中的数据有序
+        for (i = size - 1; i >= 0; --i) {
             if (elements[i] > e) {
-                elements[i+1] = elements[i];
+                elements[i + 1] = elements[i];
             } else {
                 break;
             }
         }
-        elements[i+1] = e;
+        elements[i + 1] = e;
         ++size;
     }
 }
