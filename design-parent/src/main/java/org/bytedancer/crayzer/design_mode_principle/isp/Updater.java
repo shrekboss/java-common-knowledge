6e450a04befd8afc7e716839eef9e81fc6cff1d5
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-07-12 15:55:15 +0800

    设计原则总结

--- design-parent/src/main/java/org/bytedancer/crayzer/design_mode_principle/isp/Updater.java
+++ design-parent/src/main/java/org/bytedancer/crayzer/design_mode_principle/isp/Updater.java
@@ -1,5 +1,11 @@
 package org.bytedancer.crayzer.design_mode_principle.isp;
 
+/**
+ * class_name: Updater
+ * package: org.bytedancer.crayzer.design_mode_principle.isp
+ * describe: 热更新接口
+ * @author yixiu
+ **/
 public interface Updater {
     void update();
 }
--- design-parent/src/main/java/org/bytedancer/crayzer/design_mode_principle/isp/Viewer.java
+++ design-parent/src/main/java/org/bytedancer/crayzer/design_mode_principle/isp/Viewer.java
@@ -2,6 +2,12 @@ package org.bytedancer.crayzer.design_mode_principle.isp;
 
 import java.util.Map;
 
+/**
+ * class_name: Viewer
+ * package: org.bytedancer.crayzer.design_mode_principle.isp
+ * describe: 视图查看接口
+ * @author yixiu
+ **/
 public interface Viewer {
     String outputInPlainText();
     Map<String, String> output();
