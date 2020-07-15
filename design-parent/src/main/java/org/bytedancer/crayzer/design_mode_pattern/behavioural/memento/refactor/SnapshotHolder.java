381ba83a5208961579ff6b8853c72b0cf0f50961
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-07-15 20:46:16 +0800

    备忘录模式 总结

---
+++ design-parent/src/main/java/org/bytedancer/crayzer/design_mode_pattern/behavioural/memento/refactor/SnapshotHolder.java
@@ -0,0 +1,15 @@
+package org.bytedancer.crayzer.design_mode_pattern.behavioural.memento.refactor;
+
+import java.util.Stack;
+
+public class SnapshotHolder {
+    private Stack<Snapshot> snapshots = new Stack<>();
+
+    public Snapshot popSnapshot() {
+        return snapshots.pop();
+    }
+
+    public void pushSnapshot(Snapshot snapshot) {
+        snapshots.push(snapshot);
+    }
+}
