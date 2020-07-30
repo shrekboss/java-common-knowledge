fa24b8e89e14f93e22fc0f321378a31299f6c73d
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-07-31 03:16:28 +0800

    Feign 针对方法级别设置 连接超时和读取超时

---
+++ concurrence-parent/src/main/java/org/bytedancer/crayzer/common_dev_error/httpinvoke/feignpermethodtimeout/AutoConfig.java
@@ -0,0 +1,9 @@
+package org.bytedancer.crayzer.common_dev_error.httpinvoke.feignpermethodtimeout;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "org.bytedancer.crayzer.common_dev_error.httpinvoke.feignpermethodtimeout")
+public class AutoConfig {
+}
