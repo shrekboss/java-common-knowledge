1e7d4b60c27ecf61b2ea7dd9352bd5abe89a35d5
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-08-01 00:41:06 +0800

    int and string equals

---
+++ concurrence-parent/src/main/java/org/bytedancer/crayzer/common_dev_error/coding/equals/intandstringequal/OrderQuery.java
@@ -0,0 +1,9 @@
+package org.bytedancer.crayzer.common_dev_error.coding.equals.intandstringequal;
+
+import lombok.Data;
+
+@Data
+public class OrderQuery {
+    private Integer status;
+    private String name;
+}
