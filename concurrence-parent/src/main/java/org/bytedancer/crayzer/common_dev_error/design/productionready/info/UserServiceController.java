7fe4c52511acff8ce793ba5785dd3b0cba1a3452
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-08-06 02:58:10 +0800

    对外暴露应用内部重要组件的状态 实现 InfoContributor 接口

--- concurrence-parent/src/main/java/org/bytedancer/crayzer/common_dev_error/design/productionready/info/UserServiceController.java
+++ concurrence-parent/src/main/java/org/bytedancer/crayzer/common_dev_error/design/productionready/info/UserServiceController.java
@@ -1,4 +1,4 @@
-package org.bytedancer.crayzer.common_dev_error.design.productionready.health;
+package org.bytedancer.crayzer.common_dev_error.design.productionready.info;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +15,6 @@ import java.util.concurrent.TimeUnit;
 public class UserServiceController {
     @GetMapping
     public User getUser(@RequestParam("userId") long id) {
-        //一半概率返回正确响应，一半概率抛异常
         if (ThreadLocalRandom.current().nextInt() % 2 == 0)
             return new User(id, "name" + id);
         else
