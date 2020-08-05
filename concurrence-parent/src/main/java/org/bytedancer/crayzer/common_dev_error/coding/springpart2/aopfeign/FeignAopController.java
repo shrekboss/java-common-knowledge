7a3db000bcb721fd7582081da4d22794c102d6a4
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-08-05 09:17:51 +0800

    Feign 只能切入由 Spring 自己管理的 Bean

--- concurrence-parent/src/main/java/org/bytedancer/crayzer/common_dev_error/coding/springpart2/aopfeign/FeignAopController.java
+++ concurrence-parent/src/main/java/org/bytedancer/crayzer/common_dev_error/coding/springpart2/aopfeign/FeignAopController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("feignaop")
 @RestController
-public class FeignAopConntroller {
+public class FeignAopController {
 
     @Autowired
     private Client client;
--- concurrence-parent/src/main/java/org/bytedancer/crayzer/common_dev_error/coding/springpart2/readme.md
+++ concurrence-parent/src/main/java/org/bytedancer/crayzer/common_dev_error/coding/springpart2/readme.md
@@ -1,4 +1,11 @@
 ## Spring框架：帮我们做了很多工作也带来了复杂度
 ### Feign AOP切不到的诡异案例：aopfeign
+【参考 Feign AOP切不到的诡异案例】
+
 ### Spring程序配置的优先级问题：propertysource
+
+
+
+
+
 ### （补充）替换配置属性中占位符的例子：custompropertysource
