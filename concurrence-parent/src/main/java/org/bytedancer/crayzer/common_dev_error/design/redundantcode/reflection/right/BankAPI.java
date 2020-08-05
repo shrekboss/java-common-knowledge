5e36d24598ed84aa48ae8eecd3a9b6718311549f
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-08-05 13:02:40 +0800

    涉及类结构性的通用处理，都可以按照注解+反射来减少重复代码

---
+++ concurrence-parent/src/main/java/org/bytedancer/crayzer/common_dev_error/design/redundantcode/reflection/right/BankAPI.java
@@ -0,0 +1,13 @@
+package org.bytedancer.crayzer.common_dev_error.design.redundantcode.reflection.right;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Inherited
+public @interface BankAPI {
+    String desc() default "";
+
+    String url() default "";
+}
