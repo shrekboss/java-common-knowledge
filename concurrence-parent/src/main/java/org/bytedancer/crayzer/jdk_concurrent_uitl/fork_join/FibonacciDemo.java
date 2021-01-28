88ca6872a8af9e21084509c573fe6c276aa762b2
yizhe.chen <cyz_shrek2012@foxmail.com>
2021-01-28 14:47:30 +0800

    feat(Fork/join): SumTask Example

--- concurrence-parent/src/main/java/org/bytedancer/crayzer/jdk_concurrent_uitl/fork_join/FibonacciDemo.java
+++ concurrence-parent/src/main/java/org/bytedancer/crayzer/jdk_concurrent_uitl/fork_join/FibonacciDemo.java
@@ -3,9 +3,13 @@ package org.bytedancer.crayzer.jdk_concurrent_uitl.fork_join;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
+/**
+ * @author yizhe.chen
+ */
 public class FibonacciDemo {
 
     public static void main(String[] args){
+        long startTime = System.currentTimeMillis();
         //创建分治任务线程池
         ForkJoinPool fjp = new ForkJoinPool(4);
         //创建分治任务
@@ -14,16 +18,18 @@ public class FibonacciDemo {
         Integer result = fjp.invoke(fib);
         //输出结果
         System.out.println(result);
+        System.out.println(System.currentTimeMillis() - startTime + "ms");
     }
-    //递归任务
+    /** 递归任务 */
     static class Fibonacci extends RecursiveTask<Integer> {
         final int n;
         Fibonacci(int n){this.n = n;}
 
         @Override
         protected Integer compute(){
-            if (n <= 1)
+            if (n <= 1) {
                 return n;
+            }
             Fibonacci f1 = new Fibonacci(n - 1);
             //创建子任务
             f1.fork();
